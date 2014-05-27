bd7fe2c9aa47a173fe6379e02ae51e06cce29c3c
warohotv@gmail.com <warohotv@gmail.com@f424cd9c-d50e-4efe-69b2-6e4b3e7c05d5>
2014-05-27 09:48:20 +0000

    mediaPlayer

--- tes.js
+++ tes.js
@@ -1,14 +1,11 @@
-jwplayer("player").setup({
-  playlist: "http://gdata.youtube.com/feeds/api/playlists/PL20047F0DA1618B18?max-results=50&alt=rss",
-listbar: {
-layout: 'basic',
- position: 'right',
- size: 230 },
+jwplayer("mediaPlayer").setup({
+  playlist: "http://gdata.youtube.com/feeds/api/playlists/PLa5br7p1Ra-iKeGcqeKXpIv13s5FAuqjH?max-results=50&alt=rss",
+listbar: {position: 'right',size: 250 },
+logo: {file: "http://1.bp.blogspot.com/-48Zet0D874I/UvzFP0eaQ3I/AAAAAAAAABs/l8ZM898W6TI/s1600/warohotv.png",position: 'top-right',margin: '0',link: "http://warohotv.blogspot.com/"},
 stretching: "exactfit",
-        title: '',
-        width: '100%',
-        height: '100%',
 skin: "roundster",
-sharing:' {)',
-    });
+width: '100%',
+height: '100%',
+sharing: {code: encodeURI("<iframe width='600' height='500' src='http://playerwaroho.blogspot.com/2014/05/1238.html' /></iframe>"),link: "http://warohotv.blogspot.com/" }
+});
 
