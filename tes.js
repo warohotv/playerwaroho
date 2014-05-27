957a01b56bb12ef6dc44fe180d2a241a7a57f173
warohotv@gmail.com <warohotv@gmail.com@f424cd9c-d50e-4efe-69b2-6e4b3e7c05d5>
2014-05-27 09:41:12 +0000

    tes

---
+++ tes.js
@@ -0,0 +1,66 @@
+
+function serverdianpisesha() {
+jwplayer("player").setup({
+  playlist: "http://gdata.youtube.com/feeds/api/playlists/PL20047F0DA1618B18?max-results=50&alt=rss",
+listbar: {
+layout: 'basic',
+ position: 'right',
+ size: 230 },
+stretching: "exactfit",
+        title: '',
+        width: '100%',
+        height: '100%',
+skin: "roundster",
+sharing:' {)',
+    });}
+    
+    function server1() {
+jwplayer("player").setup({
+  playlist: "http://gdata.youtube.com/feeds/api/playlists/PLDE06BA5CF8285C76?max-results=50&alt=rss",
+width: "100%",
+height: "100%",
+listbar: {'size': '220','position': 'right'},
+autostart: "true",
+primary: "flash",
+skin: "roundster",
+sharing: {
+                code:  encodeURI("<iframe width='638' height='478'  src='http://serverrrrr.blogspot.com/2014/03/embad.html'  /></iframe>"),
+                link: "http://warohotv.blogspot.com/" 
+             },
+image: "http://3.bp.blogspot.com/-xxDaCET5wYE/UA2qkdhhs5I/AAAAAAAACYk/IkjgHkJRkNY/s320/LOGOKEPSUK.png",
+    logo: {
+    file: "http://1.bp.blogspot.com/-48Zet0D874I/UvzFP0eaQ3I/AAAAAAAAABs/l8ZM898W6TI/s1600/warohotv.png",
+    position: 'top-right',
+    margin: '0',
+    link: "http://warohotv.blogspot.com/"
+    },
+    autostart: "true",
+  abouttext: "Free Broadcasting",
+            aboutlink: "http://warohotv.blogspot.com/"
+  });}
+
+   function server2() {
+jwplayer("player").setup({
+  playlist: "http://gdata.youtube.com/feeds/api/playlists/PL76AF51AAD13B6FD1?max-results=50&alt=rss",
+width: "100%",
+height: "100%",
+listbar: {'size': '220','position': 'right'},
+autostart: "true",
+primary: "flash",
+skin: "roundster",
+sharing: {
+                code:  encodeURI("<iframe  width='638'  height='478'  src='http://serverrrrr.blogspot.com/2014/03/embad.html'  /></iframe>"),
+                link: "http://warohotv.blogspot.com/" 
+             },
+image: "http://3.bp.blogspot.com/-xxDaCET5wYE/UA2qkdhhs5I/AAAAAAAACYk/IkjgHkJRkNY/s320/LOGOKEPSUK.png",
+    logo: {
+    file: "http://1.bp.blogspot.com/-48Zet0D874I/UvzFP0eaQ3I/AAAAAAAAABs/l8ZM898W6TI/s1600/warohotv.png",
+    position: 'top-right',
+    margin: '0',
+    link: "http://warohotv.blogspot.com/"
+    },
+    autostart: "true",
+  abouttext: "Free Broadcasting",
+            aboutlink: "http://warohotv.blogspot.com/"
+  });}
+
