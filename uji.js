30491b4ec2721e1a1487cebc695d864c536cf037
warohotv@gmail.com <warohotv@gmail.com@f424cd9c-d50e-4efe-69b2-6e4b3e7c05d5>
2014-06-04 09:26:58 +0000

    1

--- uji.js
+++ uji.js
@@ -1,6 +1,5 @@
 function warohotv(em) {waroho.location.replace("http://playerwaroho.blogspot.com/2014/04/"+em+".html")} 
 function warohotv2(em) {waroho.location.replace("http://embadwarohotv.blogspot.com/2014/03/"+em+".html")} 
-function warohotv3(em) {waroho.location.replace("http://serverrrrr.blogspot.com/2014/03/"+em+".html")} 
+function warohotv3(em) {waroho.location.replace("http://playerwaroho.blogspot.com/2014/06/"+em+".html")} 
 function warohotv4(em) {waroho.location.replace("http://playerwaroho.blogspot.com/2014/05/"+em+".html")}
-function udinkepsuk(em) {waroho.location.replace("http://playerwaroho.blogspot.com/2014/06/"+em+".html")} 
 warohotv('123')
