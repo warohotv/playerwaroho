b94535705594fb534bfca81fe7a803c735d38688
warohotv@gmail.com <warohotv@gmail.com@f424cd9c-d50e-4efe-69b2-6e4b3e7c05d5>
2015-02-17 21:59:16 +0000

    gggg

--- player-warohotv-03.js
+++ player-warohotv-03.js
@@ -1,4 +1,24 @@
-
+function clickwaroho() {
+  var num=0;
+        jwplayer("warohotv-oke").setup({
+'modes': [ {type: 'html5'}, {type: 'flash', src: 'https://playwaroho.googlecode.com/svn/tags/jwplayer.swf'}, {type: 'download'}],
+   playlist:playlist,
+   width: '100%',height: '100%',
+   autostart:true,
+   events:{onError:function(){ if( num <playlist.length) {num++; this.playlistNext()}}},
+   
+  });}
+  
+  function clickwaroho1() {
+  var num=0;
+        jwplayer("warohotv-oke").setup({
+'modes': [ {type: 'html5'}, {type: 'flash', src: 'https://playwaroho.googlecode.com/svn/tags/jwplayer.swf'}, {type: 'download'}],
+   playlist:playlist,
+   width: '100%',height: '100%',layout: 'basic',listbar: {position: 'right',size: 240 },
+   autostart:true,
+   events:{onError:function(){ if( num <playlist.length) {num++; this.playlistNext()}}},
+   
+  });}
 
 function clickwaroho2() {
   var num=0;
