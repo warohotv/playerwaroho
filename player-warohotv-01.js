820cda69f4e53345ee50a1c11660b38bfd856044
warohotv@gmail.com <warohotv@gmail.com@f424cd9c-d50e-4efe-69b2-6e4b3e7c05d5>
2015-02-17 21:32:15 +0000

    iyy

--- player-warohotv-01.js
+++ player-warohotv-01.js
@@ -1,4 +1,4 @@
-function clickwaroho() {
+function clickwaroho1() {
   var num=0;
         jwplayer("warohotv-oke").setup({
 'modes': [ {type: 'html5'}, {type: 'flash', src: 'https://playwaroho.googlecode.com/svn/tags/jwplayer.swf'}, {type: 'download'}],
