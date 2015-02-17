a1f102ccd3eb82bb0c8be36efb007083fdf6c8b1
warohotv@gmail.com <warohotv@gmail.com@f424cd9c-d50e-4efe-69b2-6e4b3e7c05d5>
2015-02-17 20:19:33 +0000

    hdfhydt

--- player-warohotv-01.js
+++ player-warohotv-01.js
@@ -1,4 +1,5 @@
 // JavaScript Document
+document.write('
 		var num=0;
         jwplayer("warohotv-oke").setup({
 'modes': [ {type: 'html5'}, {type: 'flash', src: 'https://playwaroho.googlecode.com/svn/tags/jwplayer.swf'}, {type: 'download'}],
@@ -8,4 +9,4 @@
 			autostart:true,
 			events:{onError:function(){ if( num <playlist.length) {num++; this.playlistNext()}}},
 			
-		})
+		})')
