0723bf055e7eb29abf7e054d70eb38569ffc34e8
warohotv@gmail.com <warohotv@gmail.com@f424cd9c-d50e-4efe-69b2-6e4b3e7c05d5>
2015-02-17 20:44:53 +0000

    khkh

---
+++ player-warohotv-02.js
@@ -0,0 +1,16 @@
+
+
+function clickwaroho() {
+jwplayer("warohotv-oke").setup({
+  playlist: "http://gdata.youtube.com/feeds/api/playlists/PL20047F0DA1618B18?max-results=50&alt=rss",
+listbar: {
+layout: 'basic',
+ position: 'right',
+ size: 230 },
+stretching: "exactfit",
+        title: '',
+        width: '100%',
+        height: '100%',
+skin: "roundster",
+sharing:' {)',
+    });}
