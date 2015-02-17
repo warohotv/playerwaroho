74794ed9de9fc66abcc0e328cc72689319c5a256
warohotv@gmail.com <warohotv@gmail.com@f424cd9c-d50e-4efe-69b2-6e4b3e7c05d5>
2015-02-17 21:02:09 +0000

    frjjt

--- player-warohotv-01.js
+++ player-warohotv-01.js
@@ -1,12 +1,9 @@
-// JavaScript Document
-document.write('
-		var num=0;
+  var num=0;
         jwplayer("warohotv-oke").setup({
 'modes': [ {type: 'html5'}, {type: 'flash', src: 'https://playwaroho.googlecode.com/svn/tags/jwplayer.swf'}, {type: 'download'}],
-			playlist:playlist,
-                        title: 'MNCTV ',
-			width: '100%',height: '100%',
-			autostart:true,
-			events:{onError:function(){ if( num <playlist.length) {num++; this.playlistNext()}}},
-			
-		})')
+   playlist:playlist,
+   width: '100%',height: '100%',
+   autostart:true,
+   events:{onError:function(){ if( num <playlist.length) {num++; this.playlistNext()}}},
+   
+  });
