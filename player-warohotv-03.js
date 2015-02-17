b64f1488b6f227d6182d4001cbaaee5f06e41ca5
warohotv@gmail.com <warohotv@gmail.com@f424cd9c-d50e-4efe-69b2-6e4b3e7c05d5>
2015-02-17 21:41:45 +0000

    hlu666

---
+++ player-warohotv-03.js
@@ -0,0 +1,12 @@
+
+
+function clickwaroho2() {
+  var num=0;
+        jwplayer("warohotv-oke").setup({
+'modes': [ {type: 'html5'}, {type: 'flash', src: 'https://playwaroho.googlecode.com/svn/tags/jwplayer.swf'}, {type: 'download'}],
+   playlist:playlist,
+   width: '100%',height: '100%',listbar: {layout: 'basic',position: 'right',size: 240 },
+   autostart:true,
+   events:{onError:function(){ if( num <playlist.length) {num++; this.playlistNext()}}},
+   
+  });}
