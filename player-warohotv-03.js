ed5db69ee4b85e6457fb248032eb9e15830c6b9c
warohotv@gmail.com <warohotv@gmail.com@f424cd9c-d50e-4efe-69b2-6e4b3e7c05d5>
2015-02-18 07:50:17 +0000

    yrdst

--- player-warohotv-03.js
+++ player-warohotv-03.js
@@ -3,7 +3,7 @@ function clickwaroho() {
         jwplayer("warohotv-oke").setup({
 'modes': [ {type: 'html5'}, {type: 'flash', src: 'https://playwaroho.googlecode.com/svn/tags/jwplayer.swf'}, {type: 'download'}],
    playlist:playlist,
-   width: '100%',height: '100%',
+   width: '100%',height: '100%',logo: {file: "http://3.bp.blogspot.com/-wjtLdp0gl3w/VI9Hu9PKiwI/AAAAAAAAB6k/8Rxat1u3KTQ/s320/logo.png",position: 'top-right',margin: '0',link: "http://warohotv.blogspot.com/"},
    autostart:true,
    events:{onError:function(){ if( num <playlist.length) {num++; this.playlistNext()}}},
    
